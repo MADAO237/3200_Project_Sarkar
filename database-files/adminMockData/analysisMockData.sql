a92f582c0c2762a736cc04b9f27d6d39cb868c64
NeyoNought47 <kaito_neyo@yahoo.com>
2025-04-10 17:52:33 -0400

    Add admin mock SQL data

---
+++ database-files/adminMockData/analysisMockData.sql
@@ -0,0 +1,3 @@
+INSERT INTO analysis (`num_orders`, `num_users`) VALUES ('192', '140');
+INSERT INTO analysis (`num_orders`, `num_users`) VALUES ('115', '228');
+INSERT INTO analysis (`num_orders`, `num_users`) VALUES ('278', '115');
