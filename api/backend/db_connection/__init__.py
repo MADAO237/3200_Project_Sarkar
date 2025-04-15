7e0ea8a5a97577ca07b4568671b68f7d612f4ede
NeyoNought47 <kaito_neyo@yahoo.com>
2025-04-15 18:04:14 -0400

    edited

--- api/backend/db_connection/__init__.py
+++ api/backend/db_connection/__init__.py
@@ -7,4 +7,15 @@ from pymysql import cursors
 
 # the parameter instructs the connection to return data 
 # as a dictionary object. 
+# as a dictionary object. 
 db = MySQL(cursorclass=cursors.DictCursor)
+
+import mysql.connector
+
+def db_connection():
+    return mysql.connector.connect(
+        host='db',
+        user='root',
+        password='password123',
+        database='projectdb'
+    )
